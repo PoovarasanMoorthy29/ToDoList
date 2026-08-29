fd64b99aeacfa910d968db2c1f2f2813b102b29b
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-08-29 10:19:32 +0530

    FEAT: include methods for CRUD operations in service layer and controller layers

--- src/main/java/com/example/ToDoList/controller/TaskController.java
+++ src/main/java/com/example/ToDoList/controller/TaskController.java
@@ -1,4 +1,40 @@
 package com.example.ToDoList.controller;
 
+import com.example.ToDoList.model.Task;
+import com.example.ToDoList.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/todolist")
 public class TaskController {
+
+    @Autowired
+    TaskService service;
+
+    @PostMapping("/create/newtask")
+    public String createNewTask(@RequestBody Task task){
+        service.createTask(task);
+        return "Task Created!";
+    }
+
+    @GetMapping("/getAllTasks")
+    public List<Task> getAllTask(){
+        return service.getAllTasks();
+    }
+
+    @PutMapping("/markTaskcompleted/{id}")
+    public String makeTaskCompleted(@PathVariable Integer id){
+        service.markTaskCompleted(id);
+        return "task completed!";
+    }
+    @DeleteMapping("/deleteTaskByID/{id}")
+    public String deleteTaskById(@PathVariable Integer id){
+        service.deleteTaskById(id);
+        return "Task Deleted!";
+    }
+
+
+
 }
---
+++ src/main/java/com/example/ToDoList/service/TaskService.java
@@ -0,0 +1,33 @@
+package com.example.ToDoList.service;
+
+import com.example.ToDoList.model.Task;
+import com.example.ToDoList.repository.ITaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    ITaskRepository iTaskRepository;
+
+    public void createTask(Task task) {
+        iTaskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return iTaskRepository.findAll();
+    }
+
+    public void markTaskCompleted(Integer id){
+        Task task = iTaskRepository.findById(id).get();
+        task.setCompleted(true);
+        iTaskRepository.save(task);
+    }
+
+    public void deleteTaskById(Integer id) {
+        iTaskRepository.deleteById(id);
+    }
+}
