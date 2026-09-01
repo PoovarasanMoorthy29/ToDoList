50c67a61b39ffc40b6e5bdb5846138d9d0e608e5
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-09-01 09:56:50 +0530

    Fix: Remove unused imports

--- src/main/java/com/example/ToDoList/service/TaskService.java
+++ src/main/java/com/example/ToDoList/service/TaskService.java
@@ -4,8 +4,6 @@ import com.example.ToDoList.model.Task;
 import com.example.ToDoList.repository.ITaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
